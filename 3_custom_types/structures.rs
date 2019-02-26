cefb74b153e4f657d392e9d25dec41997bda12e2
Iulian Gabriel Radu <iulian.radu67@gmail.com>
2019-02-27 00:26:54 +0200

    Add 3. Custom Types

---
+++ 3_custom_types/structures.rs
@@ -0,0 +1,88 @@
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
+// A unit struct
+struct Nil;
+
+// A tuple struct
+struct Pair(i32, f32);
+
+// A struct with two fields
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Structs can be reused as fields of another struct
+#[derive(Debug)]
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+fn rect_area(rect: &Rectangle) -> f32 {
+    let Rectangle { p1: Point{x: a, y: b}, p2: Point{x: c, y: d}} = rect;
+    println!("{} {} {} {}", a, b, c, d);
+
+    (a - c).abs() * (b - d).abs()
+}
+
+fn square(point: Point, len: f32) -> Rectangle {
+    let Point { x, y } = point;
+    Rectangle {
+        p1: point,
+        p2: Point{x: x + len, y: y + len}
+    }
+}
+
+fn main() {
+    // Create struct with field init shorthand
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Print debug struct
+    println!("{:?}", peter);
+
+
+    // Instantiate a `Point`
+    let point: Point = Point { x: 0.3, y: 0.4 };
+
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    // Make a new point by using struct update syntax to use the fields of our other one
+    let new_point = Point { x: 0.1, ..point };
+    // `new_point.y` will be the same as `point.y` because we used that field from `point`
+    println!("second point: ({}, {})", new_point.x, new_point.y);
+
+    // Destructure the point using a `let` binding
+    let Point { x: my_x, y: my_y } = point;
+
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        p1: Point { x: my_y, y: my_x },
+        p2: point,
+    };
+
+    // Instantiate a unit struct
+    let _nil = Nil;
+
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
+
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+
+    println!("area {}", rect_area(&_rectangle));
+    println!("square {:#?}", square(Point{x:2.0, y:3.0}, 2.0));
+}
